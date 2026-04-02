86b9a754eab2ea17bb41b61ccfe7c38787252403
kemelynfigueiredo <kemelynfigueiredo.7@gmail.com>
2026-04-01 21:04:00 -0300

    Minha primeira TabScreen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="cats"
+        options={{
+          title: 'Cats',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color}/>
+        }}
+      />
     </Tabs>
   );
 }
